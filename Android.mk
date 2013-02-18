bc3a3e9fb75e0c69ad1ed18450d87ab31a15abce
RonGokhale <cips173@gmail.com>
2013-02-18 21:55:35 +0530

    display: don't just build for msm8960
    Change-Id: I6476ffa04b77fbfbcd8464c9be703b65bb82c2f7

--- Android.mk
+++ Android.mk
@@ -1,4 +1,4 @@
-ifneq ($(filter msm8960,$(TARGET_BOARD_PLATFORM)),)
+ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
 ifeq ($(TARGET_QCOM_DISPLAY_VARIANT),)
 display-hals := libgralloc libgenlock libcopybit
 display-hals += libhwcomposer liboverlay libqdutils libexternal libqservice
